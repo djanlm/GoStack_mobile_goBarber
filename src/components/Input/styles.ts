ea63242921871502aa2c22edc3279a0a42d3e07b
Djanilson Martins <djanlm@hotmail.com>
2020-07-10 06:02:40 -0300

    validacao dos formularios

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -3,6 +3,7 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 
 interface ContainerProps {
   isFocused: boolean;
+  isErrored: boolean;
 }
 export const Container = styled.View<ContainerProps>`
   width: 100%;
@@ -16,7 +17,10 @@ export const Container = styled.View<ContainerProps>`
   flex-direction:row;
   align-items:center;
 
-  ${props => props.isFocused && css`
+  ${(props) => props.isErrored && css`
+    border-color: #C53030;
+  `}
+  ${(props) => props.isFocused && css`
     border-color: #ff9000;
   `}
 
