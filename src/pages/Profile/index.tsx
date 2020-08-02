f4becd0849fb3071991b445d0f10c2c3a0732df8
Djanilson Martins <djanlm@hotmail.com>
2020-08-02 09:50:11 -0300

    atualizacao do avatar

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,6 @@
 import React, {useRef, useCallback} from 'react';
 import  Icon from 'react-native-vector-icons/Feather';
+import ImagePicker from 'react-native-image-picker';
 
 import {
    TextInput,
@@ -122,12 +123,44 @@ const Profile: React.FC = () => {
         );
       }
     },
-    [navigation],
+    [navigation, updateUser],
   );
 
   const handleGoBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
+
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker({
+      title: 'Selecione um avatar',
+      cancelButtonTitle: 'Cancelar',
+      takePhotoButtonTitle: 'Usar câmera',
+      chooseFromLibraryButtonTitle: 'Escolha da galeria'
+    }, response => {
+      if(response.didCancel){
+       return;
+      }
+      if(response.error){
+        console.log('ImagePicker Error: ', response.error);
+        Alert.alert('Erro ao atualizar seu avatar.');
+        return;
+      }
+        const source = {uri: response.uri}
+
+        const data = new FormData();
+
+      data.append('avatar',{
+        type: 'image/jpeg',
+        name: `${user.id}.jpg`,
+        uri: response.uri,
+      });
+
+      api.patch('users/avatar', data).then((apiResponse) => {
+        updateUser(apiResponse.data);
+      });
+    },
+    );
+  }, [updateUser, user.id]);
 
   return (
     <>
@@ -145,7 +178,7 @@ const Profile: React.FC = () => {
           <Icon name="chevron-left" size={24} color="#999591" />
         </BackButton>
 
-        <UserAvatarButton onPress={() => {}}>
+        <UserAvatarButton onPress={handleUpdateAvatar}>
           <UserAvatar source={{uri: user.avatar_url}}/>
         </UserAvatarButton>
         <View>
