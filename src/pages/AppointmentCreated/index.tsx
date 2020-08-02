8caee82f5e7091cf9bc22b31a4cc4a582cebb227
Djanilson Martins <djanlm@hotmail.com>
2020-08-01 21:05:06 -0300

    criando estrutura da tela de sucesso

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { View} from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation(); // reset é como o navigate mais reseta as paginas anteriores, assim nao da pra voltar pra elas
 
+  const handleOkPressed = useCallback(() =>{
+    reset({
+      routes: [
+        {
+          name: 'Dashboard'
+        }
+      ],
+      index: 0
+    })
+
+  }, [reset]);
   return(
-    <View />
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+      <Title> Agendamento concluido</Title>
+      <Description> Terça, dia 14 de março de 2020 as 12:00h </Description>
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   )
 }
 
