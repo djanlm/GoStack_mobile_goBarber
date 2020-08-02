6a16315bdca19c55fb91bf492c783620706b86f8
Djanilson Martins <djanlm@hotmail.com>
2020-08-01 23:59:30 -0300

    formatando data

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,5 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import {
   Container,
@@ -8,10 +11,16 @@ import {
   OkButton,
   OkButtonText,
 } from './styles';
-import { useNavigation } from '@react-navigation/native';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation(); // reset é como o navigate mais reseta as paginas anteriores, assim nao da pra voltar pra elas
+  const { params } = useRoute(); // a data veio porparametro
+
+  const routeParams = params as RouteParams;
 
   const handleOkPressed = useCallback(() =>{
     reset({
@@ -24,11 +33,20 @@ const AppointmentCreated: React.FC = () => {
     })
 
   }, [reset]);
+
+  const formattedDate = useMemo(() =>{
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR }
+    );
+  }, [routeParams.date]);
+
   return(
     <Container>
       <Icon name="check" size={80} color="#04d361" />
       <Title> Agendamento concluido</Title>
-      <Description> Terça, dia 14 de março de 2020 as 12:00h </Description>
+      <Description> {formattedDate} </Description>
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
       </OkButton>
